5f3970c982d4d4d3db09a126ee00341ebb4a2658
Brian Nyaga <briannyagak@gmail.com>
2023-11-30 18:17:43 +0300

    pudh

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
+import add_0
 
 if __name__ == "__main__":
+    a = 1
+    b = 2
     """
 
     Prints the result of the addition between two numbers
 
     """
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print(f'{a} + {b} = {add_0.add(a, b)}')
