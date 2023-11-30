4135e51d32fce4d712101df6f246cdafac5cbd11
Brian Nyaga <briannyagak@gmail.com>
2023-11-30 17:37:11 +0300

    corrections

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
-"""
+
+    a = 1
+    b = 2
+    """
 
     Prints the result of the addition between two numbers
 
-"""
-print(f'{a} + {b} = {add_0.add(a, b)}')
+    """
+    print(f'{a} + {b} = {add(a, b)}')
